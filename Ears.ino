23d56caac288d0ea6351cbaec2eae961e6a3294f
Brent Lewis <coder0xff@gmail.com>
2013-06-02 16:52:44 -0700

    Bug fixes and debug  code

--- Ears.ino
+++ Ears.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 //configuration
-#define AUTO_MODE_PIN 0 //RX
+#define AUTO_MODE_PIN A0
 #define MANUAL_DOWN_PIN 2
 #define MANUAL_TWITCH_PIN 4
 #define LEFT_MOTOR_POS_PWM_PIN 3
@@ -18,8 +18,8 @@
 
 //constants
 enum ear {
-    LEFT_EAR,
-    RIGHT_EAR
+    LEFT_EAR = 0,
+    RIGHT_EAR = 1
 };
 
 void set_ear_velocity(ear e, char velocity) {
@@ -56,12 +56,23 @@ unsigned char stop_switch_is_activated(ear e) {
 }
 
 enum orient_step {
-    ORIENT_START,
-    ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION,
-    ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION,
-    ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION,
-    ORIENT_FAILED,
-    ORIENT_SUCCEEDED
+    ORIENT_START = 0,
+    ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION = 1,
+    ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION = 2,
+    ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION = 3,
+    ORIENT_FAILED = 4,
+    ORIENT_SUCCEEDED = 5
+};
+
+char const * ear_names[] = {"Left", "Right"};
+
+char const * orient_step_names[] = {
+    "ORIENT_START",
+    "ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION",
+    "ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION",
+    "ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION",
+    "ORIENT_FAILED",
+    "ORIENT_SUCCEEDED"
 };
 
 orient_step orient_step_action(ear e, orient_step step, unsigned long *timeout_time) {
@@ -69,6 +80,8 @@ orient_step orient_step_action(ear e, orient_step step, unsigned long *timeout_t
     case ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION:
         if (millis() > *timeout_time) {
             set_ear_velocity(e, 0);
+            Serial.print(ear_names[e]); Serial.print(": ");
+            Serial.println("ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION timed out. ORIENT_FAILED");
             return ORIENT_FAILED;
         }
         set_ear_velocity(e, 127);
@@ -80,6 +93,9 @@ orient_step orient_step_action(ear e, orient_step step, unsigned long *timeout_t
         }
     case ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION:
         if (millis() > *timeout_time) {
+            Serial.print(ear_names[e]); Serial.print(": ");
+            Serial.println("ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION timed out. ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION");
+            *timeout_time = millis() + ORIENT_TIMEOUT_MILLS;
             return ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION;
         }
         set_ear_velocity(e, -128);
@@ -92,14 +108,20 @@ orient_step orient_step_action(ear e, orient_step step, unsigned long *timeout_t
     case ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION:
         if (millis() > *timeout_time) {
             set_ear_velocity(e, 0);
+            Serial.print(ear_names[e]); Serial.print(": ");
+            Serial.println("ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION timed out. ORIENT_FAILED");
             return ORIENT_FAILED;
         }
         set_ear_velocity(e, 127);
         if (stop_switch_is_activated(e)) {
+            *timeout_time = millis() + ORIENT_TIMEOUT_MILLS;
             return ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION;
         } else {
             return ORIENT_SEARCH_FOR_STOP_IN_POSITIVE_DIRECTION;
         }
+    case ORIENT_SUCCEEDED:
+        set_ear_velocity(e, 0);
+        return ORIENT_SUCCEEDED;
     default:
         set_ear_velocity(e, 0);
         return ORIENT_FAILED;
@@ -111,19 +133,22 @@ orient_step orient_step_action(ear e, orient_step step, unsigned long *timeout_t
 //If quick is true, it will finish immediately if it starts with the switch active - for end of sequences
 void orient_ears(unsigned char quick) {
     orient_step left_orient_step = (!quick && digitalRead(LEFT_MOTOR_STOP_PIN)) ? ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION : ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION;
+    Serial.print("left: "); Serial.println(orient_step_names[left_orient_step]);
     unsigned long left_timeout_time = millis() + ORIENT_TIMEOUT_MILLS;
     orient_step right_orient_step = (!quick && digitalRead(RIGHT_MOTOR_STOP_PIN)) ? ORIENT_SEARCH_FOR_NOT_STOP_IN_POSITIVE_DIRECTION : ORIENT_SEARCH_FOR_STOP_IN_NEGATIVE_DIRECTION;
+    Serial.print("right: "); Serial.println(orient_step_names[right_orient_step]);
     unsigned long right_timeout_time = millis() + ORIENT_TIMEOUT_MILLS;
-    while ((left_orient_step != ORIENT_SUCCEEDED && left_orient_step != ORIENT_FAILED)|| (right_orient_step != ORIENT_SUCCEEDED && right_orient_step != ORIENT_SUCCEEDED)) {
+    while ((left_orient_step != ORIENT_SUCCEEDED && left_orient_step != ORIENT_FAILED) || (right_orient_step != ORIENT_SUCCEEDED && right_orient_step != ORIENT_FAILED)) {
         left_orient_step = orient_step_action(LEFT_EAR, left_orient_step, &left_timeout_time);
         right_orient_step = orient_step_action(RIGHT_EAR, right_orient_step, &right_timeout_time);
     }
     if (left_orient_step == ORIENT_FAILED || right_orient_step == ORIENT_FAILED) {
+        Serial.println("Orientation failed");
         pinMode(13, OUTPUT);
         while(1) {
-            digitalWrite(13, 1);
+            digitalWrite(13, HIGH);
             delay(1000);
-            digitalWrite(13, 0);
+            digitalWrite(13, LOW);
             delay(1000);
         }
     }
@@ -155,6 +180,7 @@ void ear_twitch_sequence() {
 
 void setup()
 {
+    Serial.begin(9600);
     pinMode(AUTO_MODE_PIN, INPUT);
     pinMode(MANUAL_DOWN_PIN, INPUT);
     pinMode(MANUAL_TWITCH_PIN, INPUT);
@@ -162,6 +188,7 @@ void setup()
     pinMode(LEFT_MOTOR_STOP_PIN, INPUT);
     pinMode(RIGHT_MOTOR_ENABLE_PIN, OUTPUT);
     pinMode(RIGHT_MOTOR_STOP_PIN, OUTPUT);
+    Serial.println("Pins setup, orienting ears.");
     orient_ears(false);
 }
 
